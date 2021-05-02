1e0e63e8f5637fc8d35844a938fb20eddf6fd010
lerato1ofone <letsepe100@gmail.com>
2021-05-02 08:22:12 +0200

    configured env variables in next config

--- next.config.js
+++ next.config.js
@@ -9,12 +9,26 @@ module.exports = {
     STRAPI_URL: process.env.STRAPI_URL,
     NEXT_PUBLIC_IMAGES_DOMAIN: process.env.NEXT_PUBLIC_IMAGES_DOMAIN,
     HOST: process.env.NEXT_PUBLIC_HOST,
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+    REDIRECT_URI: process.env.REDIRECT_URI,
+    REFRESH_TOKEN: process.env.REFRESH_TOKEN,
+    MY_EMAIL: process.env.MY_EMAIL,
+    EMAIL_SERVICE: process.env.EMAIL_SERVICE,
+    AUTH_TYPE: process.env.AUTH_TYPE
   },
 
   publicRuntimeConfig: {
     STRAPI_URL: process.env.STRAPI_URL,
     NEXT_PUBLIC_IMAGES_DOMAIN: process.env.NEXT_PUBLIC_IMAGES_DOMAIN,
     HOST: process.env.NEXT_PUBLIC_HOST,
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+    REDIRECT_URI: process.env.REDIRECT_URI,
+    REFRESH_TOKEN: process.env.REFRESH_TOKEN,
+    MY_EMAIL: process.env.MY_EMAIL,
+    EMAIL_SERVICE: process.env.EMAIL_SERVICE,
+    AUTH_TYPE: process.env.AUTH_TYPE
   },
 
   images: {
