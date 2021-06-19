e11085923e989951ce84138e2e3c7ea1eaae5b80
lerato1ofone <letsepe100@gmail.com>
2021-06-19 21:38:58 +0200

    Worked on hero section layout

--- components/Hero.js
+++ components/Hero.js
@@ -1,7 +1,20 @@
+import styles from '../styles/Hero.module.scss'
+import Link from 'next/link'
+import Image from 'next/image'
+
 const Hero = () => {
     return (
-        <div>
-            Hero
+        <div className={styles.container}>
+
+            <div className={styles['left-content']}>
+             
+            
+            </div>
+
+            <div className={styles['right-content']}>
+            
+            </div>
+            
         </div>
       );
 }
