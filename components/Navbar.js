9b57d18bda8697de395b7d78e5def225c9d2584e
lerato1ofone <letsepe100@gmail.com>
2021-06-22 23:57:32 +0200

    Updated Blogs Component styles, and added some helper functions and Constants for months to map month number to month short text

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,7 @@
 import styles from '../styles/Navbar.module.scss'
 import Link from 'next/link'
 import Image from 'next/image'
+import SocialIcons from './SocialIcons'
 
 const Navbar = () => {
     return ( 
@@ -36,13 +37,7 @@ const Navbar = () => {
                 </nav>
             </div>
 
-            <div className={styles['social-links']}>
-                <ul>
-                    <li><a target="_blank" href="https://www.github.com/lerato1ofone" rel="noopener noreferrer"><Image src="/icons/github.svg" alt="github" width="30" height="30"/></a></li>
-                    <li><a target="_blank" href="https://www.twitter.com/lerato1ofone" rel="noopener noreferrer"><Image src="/icons/twitter.svg" alt="twitter" width="30" height="30"/></a></li>
-                    <li><a target="_blank" href="https://www.linkedin.com/in/lerato-letsepe-1312b5156/" rel="noopener noreferrer"><Image src="/icons/linkedin.svg" alt="linkedin" width="30" height="30"/></a></li>
-                </ul>
-            </div>
+            <SocialIcons />
             
         </div>
     );
--- src/GeneralHelpers.js
+++ src/GeneralHelpers.js
@@ -1,3 +1,5 @@
+import { months } from "./services/models/Constants";
+
 export const isInDev = () =>{
     const inDev = new Date();
     const launchDate = new Date('July 02, 2021 12:00:00');
@@ -7,4 +9,14 @@ export const isInDev = () =>{
 export const blurDataUrl = () =>{
     const data = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAMAAAADCAYAAABWKLW/AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAAEnQAABJ0Ad5mH3gAAAAYSURBVBhXY2RgYPgPxGDABKXBAInDwAAAI3kBBSpozL8AAAAASUVORK5CYII=";
     return data;
+}
+
+export const getDate = (date) => {
+    let formatedDate = new Date(date);
+    formatedDate =  `${formatedDate.getDate()} ${getMonth(formatedDate.getMonth())} ${formatedDate.getFullYear()}`
+    return formatedDate;
+}
+
+export const getMonth = (monthNumber) => {
+    return months[monthNumber];
 }
