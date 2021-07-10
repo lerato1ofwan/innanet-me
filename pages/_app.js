4d9ed239628f97fdfe8e96988a971255f8f686ce
lerato1ofone <letsepe100@gmail.com>
2021-07-10 20:23:25 +0200

    Added Animate presence wrapper in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import '../styles/globals.scss'
 import Layout from '../components/Layout'
+import { AnimatePresence } from 'framer-motion';
 
 function MyApp({ Component, pageProps }) {
+  console.log(Component)
   return (
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence exitBeforeEnter>
+        <Component {...pageProps} key={Component}/>
+      </AnimatePresence>
     </Layout>
   )
 }
