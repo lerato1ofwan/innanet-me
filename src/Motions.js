351e9b4cae38d4c799d5280ef7672faf0430f360
lerato1ofone <letsepe100@gmail.com>
2021-07-10 23:21:54 +0200

    Updated contact page input and message input fields width

--- src/Motions.js
+++ src/Motions.js
@@ -1,5 +1,29 @@
 export const easing = [0.6, -0.05, 0.01, 0.99]
 
+export const opacity = {
+    initial: { 
+        opacity: 0 
+    },
+    animate: {
+        opacity: 1,
+        transition: {
+            delay: 0.5
+        }
+    }
+}
+
+export const secondOpacity = {
+    initial: { 
+        opacity: 0 
+    },
+    animate: {
+        opacity: 1,
+        transition: {
+            delay: 1
+        }
+    }
+}
+
 export const fadeInUp = {
     initial: {
         y: -60,
@@ -14,3 +38,27 @@ export const fadeInUp = {
         }
     }
 }
+
+export const yfadeInUp = {
+    hidden: { y: 60, opacity: 0 },
+    show: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        ease: easing,
+        staggerChildren: 0.4,
+        duration: 0.4
+      }
+    }
+  };
+  
+export const stagger = {
+    hidden: { 
+        y: 60, 
+        opacity: 0 
+    },
+    show: { 
+        y: 0, 
+        opacity: 1 
+    }
+};
