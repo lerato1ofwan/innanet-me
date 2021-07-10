1ab47e0dbbea0a762bcd7f29384045ad271c61f3
lerato1ofone <letsepe100@gmail.com>
2021-07-10 23:19:36 +0200

    Added stagger effect for blogs on home screen

--- components/Blogs.js
+++ components/Blogs.js
@@ -2,6 +2,8 @@ import styles from '../styles/Blogs.module.scss'
 import PageTitle from './PageTitle';
 import Button from './Button';
 import PostPreviewCard from './PostPreviewCard'
+import { motion } from 'framer-motion';
+import { stagger, yfadeInUp } from '../src/Motions';
 
 const Blogs = ({posts}) => {
 
@@ -14,11 +16,15 @@ const Blogs = ({posts}) => {
                     <p className={styles.subtitle}>Latest articles <span>→</span></p>
                 </div>
 
-                <div className={styles['blogs-container']}>
+                <motion.div className={styles['blogs-container']}
+                    variants={yfadeInUp} initial="hidden" animate="show"
+                >
                     {posts.map((post) => (
+                        <motion.div variants={stagger}>
                             <PostPreviewCard key={post.id} post={post} />
+                        </motion.div>
                     ))}
-                </div>
+                </motion.div>
                 
                 <div className={styles.more}>
                     <Button text="View All" type="" link="blog" />
