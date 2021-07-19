17b7f93f8366c5583812f5d9418819cd49cdda94
lerato1ofone <letsepe100@gmail.com>
2021-07-20 01:09:55 +0200

    Updated app.js to fix Framer motion removing styles before page exit animations in prod env

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,55 @@
+import { useEffect } from "react";
+import { AnimatePresence } from "framer-motion";
+import Router from "next/router";
 import '../styles/globals.scss'
 import Layout from '../components/Layout'
-import { AnimatePresence } from 'framer-motion';
 
-function MyApp({ Component, pageProps }) {
+function MyApp(props) {
+  const { Component, pageProps, router } = props;
+
+  let copies = [];
+
+  useEffect(() => {
+    Router.events.on("beforeHistoryChange", onLoad);
+    return () => {
+      Router.events.off("beforeHistoryChange", onLoad);
+    };
+  }, []);
+
+  const onLoad = () => {
+    // Create a clone of every <style> and <link> that currently affects the page. It doesn't matter
+    // if Next.js is going to remove them or not since we are going to remove the copies ourselves
+    // later on when the transition finishes.
+    const nodes = document.querySelectorAll(
+      "link[rel=stylesheet], style:not([media=x])"
+    );
+    copies = [...nodes].map((el) => el.cloneNode(true));
+
+    for (let copy of copies) {
+      // Remove Next.js' data attributes so the copies are not removed from the DOM in the route
+      // change process.
+      copy.removeAttribute("data-n-p");
+      copy.removeAttribute("data-n-href");
+
+      // Add duplicated nodes to the DOM.
+      document.head.appendChild(copy);
+    }
+  };
+
+  const onExit = () => {
+    for (let copy of copies) {
+      // Remove previous page's styles after the transition has finalized.
+      document.head.removeChild(copy);
+    }
+  };
+
   return (
     <Layout>
-      <AnimatePresence exitBeforeEnter>
-        <Component {...pageProps} key={Component}/>
-      </AnimatePresence>
+      <AnimatePresence exitBeforeEnter={true} onExitComplete={onExit}>
+      <Component {...pageProps} key={router.route} />
+    </AnimatePresence>
     </Layout>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
