1927c587bed078b06535c2645f1d81938f29dec4
lerato1ofone <letsepe100@gmail.com>
2021-07-22 23:04:45 +0200

    Added custom cursor file (@TODO: Imlementation)

---
+++ components/CustomCursor.js
@@ -0,0 +1,12 @@
+import styles from '../styles/CustomCursor.module.scss';
+
+const CustomCursor = () => {
+
+    return (
+      <div className={styles.cursor}>
+        
+      </div>
+    );
+  };
+ 
+export default CustomCursor;
