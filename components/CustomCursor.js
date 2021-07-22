cf17e1d07173bc04d3533a63caa5ce6d322148be
lerato1ofone <letsepe100@gmail.com>
2021-07-22 23:12:47 +0200

    Implement mouse positions with useState and useRef hooks

--- components/CustomCursor.js
+++ components/CustomCursor.js
@@ -1,6 +1,33 @@
+import { useRef, useEffect } from 'react';
 import styles from '../styles/CustomCursor.module.scss';
 
 const CustomCursor = () => {
+
+  const cursorRef = useRef(null);
+
+  useEffect(() => {
+    if (cursorRef.current == null || cursorRef == null)
+        return;
+
+    document.addEventListener('mousemove', e => {
+        if (cursorRef.current == null)
+              return;
+        cursorRef.current.setAttribute("style", "top: " + (e.pageY) + "px; left: " + (e.pageX) + "px;")
+    });
+
+    document.addEventListener('click', () => {
+      if (cursorRef.current == null)
+          return;
+
+      cursorRef.current.classList.add("expand");
+
+      setTimeout(() => {
+          if (cursorRef.current == null)
+                return;
+          cursorRef.current.classList.remove("expand");
+      }, 500);
+    });
+  }, []);
 
     return (
       <div className={styles.cursor}>
