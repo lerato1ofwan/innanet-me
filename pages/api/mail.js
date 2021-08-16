bd0f4ece5b06c41eb3c6a9e5bc8d92aa2c1cd0fb
Lerato1OFONE <letsepe100@gmail.com>
2021-08-16 13:51:59 +0200

    Reworks mail api to use sendgrid instead of nodemailer

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,59 +1,9 @@
-const nodemailer = require("nodemailer");
-const { google } = require("googleapis");
-
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
-const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 const MY_EMAIL = process.env.MY_EMAIL;
-const EMAIL_SERVICE = process.env.EMAIL_SERVICE;
-const AUTH_TYPE = process.env.AUTH_TYPE;
-
-const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
-oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN});
-
-async function sendMail(body, output) {
-    try {
-        const accessToken = await oAuth2Client.getAccessToken();
-
-        // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport({
-            service: EMAIL_SERVICE,
-            auth: {
-                type: AUTH_TYPE,
-                user: MY_EMAIL,
-                clientId: CLIENT_ID,
-                clientSecret: CLIENT_SECRET,
-                refreshToken: REFRESH_TOKEN,
-                accessToken: accessToken
-            }
-        });
-
-        let mailOptions = {
-            from: `"${body.name}" <${body.email}>`,
-            to: MY_EMAIL,
-            subject: body.subject, // Subject line
-            text: body.message, // plain text body
-            html: output, // html body
-        }
-        
-        // send mail with defined transport object
-        let info = await transporter.sendMail(mailOptions, (error, info) => {
-            if(error)
-                return console.log(error);
-            console.log("Message sent: %s", info.messageId);
-            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-        });
-
-    } catch (error) {
-        return error;
-    }
-}
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 export default async(req, res) => {
-
     const body = JSON.parse(req.body);
-    console.log(body);
 
     const output = `
         <p>You've got a new ${body.type} request from your website</p>
@@ -66,12 +16,22 @@ export default async(req, res) => {
         <p>${body.message}</p>
     `;
 
-    sendMail(body, output).then(
-        result => {
-            res.status(200).json({status: 'Ok'});
-        },
-        error => {
-            console.log(error.message);
+    const msg = {
+        to: MY_EMAIL, 
+        from: MY_EMAIL,
+        subject: body.subject,
+        text: body.message,
+        html: output,
+      }
+
+    sgMail.send(msg).then(
+        () => {
+            console.log('Email sent');
+        })
+        .catch((error) => {
+            console.error(error.response.body);
         }
     );
+
+    res.status(200).json({status: 'Ok'})
 }
